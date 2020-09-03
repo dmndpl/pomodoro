639bd5017e8a795058a7c3271abb8690a1067285
Max Diamandopol <max.diamandopol@intenthq.com>
2020-09-03 11:36:24 +0100

    Change to point to system host file, removed logs

--- hostname_utils.sh
+++ hostname_utils.sh
@@ -1,16 +1,16 @@
 function create_blocked_hostnames {
-  cp ./hosts ./hosts.bk
-  echo "# Added by pomodoro.sh" >> hosts.bk
+  cp /etc/hosts /etc/hosts.bk
+  echo "# Added by pomodoro.sh" >> /etc/hosts.bk
   while read -r line
   do
-    echo "127.0.0.1   ${line}" >> hosts.bk
+    echo "127.0.0.1   ${line}" >> /etc/hosts.bk
   done < banned_hostnames.txt
 }
 
 function swap_hostnames {
- mv ./hosts ./hosts.swap && mv ./hosts.bk ./hosts && mv ./hosts.swap ./hosts.bk
+ mv /etc/hosts /etc/hosts.swap && mv /etc/hosts.bk /etc/hosts && mv /etc/hosts.swap /etc/hosts.bk
 }
 
 function remove_blocked_hostnames {
-  rm ./hosts.bk
+  rm /etc/hosts.bk
 }
--- notification_utils.sh
+++ notification_utils.sh
@@ -4,7 +4,6 @@ function displayWorkSessionNotification {
   notificationString="Pomodoro number $1"
   notificationTitle="Pomodoro Started"
   notificationString="display notification \"${notificationString}\" with title \"${notificationTitle}\" sound name \"Tink\""
-  echo $notificationString
   osascript -e "${notificationString}"
 }
 
@@ -12,6 +11,5 @@ function displayPauseSessionNotification {
   notificationString="Pause number $1"
   notificationTitle="Pause Started"
   notificationString="display notification \"${notificationString}\" with title \"${notificationTitle}\" sound name \"Tink\""
-  echo $notificationString
   osascript -e "${notificationString}"
 }
